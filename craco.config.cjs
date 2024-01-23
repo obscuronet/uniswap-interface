6c985f3c644063b5866d2710d0e3a13cd428e727
otherview <otherview@gmail.com>
2024-01-23 16:15:24 +0000

    testnet name

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,13 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   babel: {
     plugins: ['@vanilla-extract/babel-plugin'],
   },
   webpack: {
-    plugins: [new VanillaExtractPlugin()],
+    plugins: [
+        new VanillaExtractPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.TESTNET_NAME': JSON.stringify(process.env.TESTNET_NAME)
+      })
+    ],
     configure: (webpackConfig) => {
       const instanceOfMiniCssExtractPlugin = webpackConfig.plugins.find(
         (plugin) => plugin instanceof MiniCssExtractPlugin
